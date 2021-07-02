79d769314079bf3c0df29587d7d35a019de7a265
Mohit Tekwani <83222773+tekwaniMohit@users.noreply.github.com>
2021-07-02 17:39:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+$("h1").css("font-size", "5rem");
+
+$("button").click(function(){
+    $("h1").css("color" , "grey")
+});
+
+
+
+$("button").css("font-size","3rem");
+
+
+$("button").click(function(){
+ var audio = new Audio('audio/senorita.mp3');
+ audio.play()
+
+});
